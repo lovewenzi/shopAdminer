05fbfc05b37ec6b3d6d5028540397f17b4ba25a9
lovewenzi <372485149@qq.com>
2019-03-19 10:15:09 +0800

    商城php后台

---
+++ public/vueapi/index.php
@@ -0,0 +1,8 @@
+<?php
+// [ 应用入口文件 ]
+define('BIND_MODULE', 'vueapi');
+// 定义应用目录
+define('APP_PATH', __DIR__ . '/../../application/');
+//define('APP_PATH', __DIR__ .);
+// 定义应用缓存目录
+require __DIR__ . '/../../thinkphp/start.php';
